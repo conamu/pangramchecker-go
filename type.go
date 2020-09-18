978eb6fab379f8dc001567e172ddc6c60bcb7e4d
conamu <constantin.amundsen@gmail.com>
2020-09-18 17:58:57 +0200

    updated types and created custom map type for present characters list

--- type.go
+++ type.go
@@ -1,10 +1,12 @@
 package main
 
+type customMap map[string]bool
+
 type pangram struct {
 	sentence       string
 	missing        []string
-	nMissing       string
 	pangram        bool
 	perfectPangram bool
 	data           string
+	presentChars   customMap
 }
